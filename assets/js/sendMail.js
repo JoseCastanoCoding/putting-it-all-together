a332268718c3ecc8f138d3c52f73f83435c43dc4
JoseCastanoCoding <j.albertocast@gmail.com>
2021-04-07 23:10:33 +0000

    final commit

--- assets/js/sendMail.js
+++ assets/js/sendMail.js
@@ -4,12 +4,13 @@ function sendMail(contactForm) {
         "from_email": contactForm.emailaddress.value,
         "project_request": contactForm.projectsummary.value
     })
-    .then(
-        function(response) {
-            console.log("SUCCESS", response);
-        },
-        function(error) {
-            console.log("FAILED", error);
-        });
-    
+        .then(
+            function (response) {
+                console.log("SUCCESS", response);
+            },
+            function (error) {
+                console.log("FAILED", error);
+            });
+    return false;
+
 }
